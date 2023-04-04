3ab3387f588685a3a30bc726849011d1fb668e13
Michael de Jong <mj22@curio.nl>
2023-04-04 14:59:44 +0200

    initial commit

---
+++ controller/getFormData.php
@@ -0,0 +1,6 @@
+<?php
+
+$name = $_GET['name'];
+$surname = $_GET['surname'];
+
+echo 'Hello, ' . $name . ' ' . $surname;
---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Basic Linting</title>
+</head>
+<body>
+    <h1>PHP Basic Linting</h1>
+    <hr/>
+    <form action="controller/getFormData.php" method="get">
+        <input type="text" name="name" placeholder="Naam" />
+        <input type="text" name="surname" placeholder="Voornaam" />
+        <input type="submit" value="Versturen" />
+    </form>
+</body>
+</html>
